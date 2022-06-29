5272ae3e2e980e898ef8e12df1126455979f1858
imranahmed1990 <imranmarin1990@gmail.com>
2022-06-29 09:35:38 +0600

    added

--- src/Pages/Dashboard/MyUsers.js
+++ src/Pages/Dashboard/MyUsers.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useState } from 'react';
 
 const MyUsers = () => {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/user')
+            .then(res => res.json())
+            .then(data => setUsers(data))
+    }, [])
     return (
         <div>
-            <h2>My Users</h2>
+            <h2>My Users{users.length}</h2>
         </div>
     );
 };
